7514e5f31fe4c9b808e0c5858159662b2224e416
Ethan Huret <ethan.huret@etu.unistra.fr>
2022-11-25 10:35:13 +0100

    test

--- defs.h
+++ defs.h
@@ -121,7 +121,6 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-void test(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
